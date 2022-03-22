2a192edf14c69d460eb87f67a2b43119babb451c
Shifra Wagshall <shifrawagshall@gmail.com>
2022-03-22 00:32:43 -0400

    Created connection.js and connected to mongo.

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+const { connect, connection } = require('mongoose');
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/studentsDB';
+
+connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true});
+
+module.exports = connection;
