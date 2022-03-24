8d3444329c265a1e3f97e2c6bd9dab18e37c3ca1
Shifra Wagshall <shifrawagshall@gmail.com>
2022-03-23 23:37:07 -0400

    Added the code to models/user.js and models/index.js.

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Thought = require('./thought');
+const User = require('./user');
+const Reaction = require('./reaction');
+
+module.exports = { Thought, User, Reaction };
---
+++ models/user.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
+  },
+  thoughts: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought'
+      }
+    ],
+  friends: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+      }
+  ]
+  },
+  { 
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
+}
+);
+
+const User = model('User', UserSchema);
+
+// get total count of friends
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+
+ 
+module.exports = User;
