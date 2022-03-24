8aced7d096510b4022147dd5c9257e6cb84db567
Shifra Wagshall <shifrawagshall@gmail.com>
2022-03-23 23:55:35 -0400

    Created the routes for all the users controllers.

---
+++ routes/api/user-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    addFriend,
+    updateUser,
+    deleteUser,
+    deleteFriend
+} = require('../../controllers/userController');
+
+router
+    .route('/')
+    .get(getUser)
+    .post(createUser);
+
+router
+    .route('/:id')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser)
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend)
+
+module.exports = router;
