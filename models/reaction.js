9612a47cf5d9a75196abc91ce3f3f7cd8742863a
Shifra Wagshall <shifrawagshall@gmail.com>
2022-03-23 23:32:03 -0400

    Created the model for reaction.js.

---
+++ models/reaction.js
@@ -0,0 +1,34 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+
+const ReactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now(),
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  );
+
+  module.exports = ReactionSchema; 
